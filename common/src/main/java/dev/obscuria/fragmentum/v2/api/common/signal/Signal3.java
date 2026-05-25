935a834de78239f4db2246b60f1123d800dee200
Obscuria <obscurialithium@gmail.com>
2026-05-25 14:47:17 -0300

    fix: correct return type in Signal3.create() method

--- common/src/main/java/dev/obscuria/fragmentum/v2/api/common/signal/Signal3.java
+++ common/src/main/java/dev/obscuria/fragmentum/v2/api/common/signal/Signal3.java
@@ -5,8 +5,8 @@ import dev.obscuria.fragmentum.v2.core.common.signal._Signal3;
 @SuppressWarnings("unused")
 public interface Signal3<P1, P2, P3> extends Signal<Signal3.Listener<P1, P2, P3>> {
 
-    static <P1, P2, P3> Signal2<P1, P2> create() {
-        return new _Signal2<>();
+    static <P1, P2, P3> Signal3<P1, P2, P3> create() {
+        return new _Signal3<>();
     }
 
     void emit(P1 p1, P2 p2, P3 p3);
